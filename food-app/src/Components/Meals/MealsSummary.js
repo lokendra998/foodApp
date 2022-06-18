3541720250471782403bee8ff07f7e9c9cfaa18f
Lokendra <lokendraakbarpur@gmail.com>
2022-06-17 22:34:36 -0700

    tree clean

--- food-app/src/Components/Meals/MealsSummary.js
+++ food-app/src/Components/Meals/MealsSummary.js
@@ -1,6 +1,18 @@
 import classes from './MealsSummary.module.css';
 
 const MealsSummary = () => {
+
+fetch("https://newsapi.org/v2/everything?q=tesla&from=2022-05-06&sortBy=publishedAt&apiKey=df96d27cf6b44195805feb36d79d9dfa").then((res)=>{
+  res.json()
+}).then((data)=>{
+
+console.log(data)
+})
+
+const clickedhandle=()=>{
+  console.log("cliked!!")
+};
+
   return (
     <section className={classes.summary}>
       <h2>Delicious Food, Delivered To You</h2>
@@ -12,6 +24,8 @@ const MealsSummary = () => {
         All our meals are cooked with high-quality ingredients, just-in-time and
         of course by experienced chefs!
       </p>
+
+      <button type="button" className="btn btn-primary" onClick={clickedhandle}>Get map</button>
     </section>
   );
 };
